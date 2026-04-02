1c2c06cc70b52b4885d6f8b670768cf199cf027e
tnxp21♫⋆｡♪ ₊˚♬ﾟ. <tnxp2001@gmail.com>
2026-04-03 04:16:21 +0700

    small stripe and google font adjust

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -1,7 +1,8 @@
 import Stripe from 'stripe';
 import '../../../envConfig.js';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const API_KEY = process.env.STRIPE_SECRET_KEY;
+const stripe = new Stripe(API_KEY);
 
 export async function POST(request) {
     try {
